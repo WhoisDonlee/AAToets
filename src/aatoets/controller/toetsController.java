83017eba614bb1ec6546d84b01a3ac182837e79c
Dennis Dollee <dennisdollee@gmail.com>
2018-06-15 13:26:08 +0200

    Toetscontroller togglegroup gettext

--- src/aatoets/controller/toetsController.java
+++ src/aatoets/controller/toetsController.java
@@ -1,4 +1,28 @@
 package aatoets.controller;
 
-public class toetsController {
+import aatoets.data.OptieClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.io.IOException;
+
+public class toetsController extends Controller {
+    @FXML
+    ToggleGroup aantal;
+
+
+    public void loadBeginScene(ActionEvent actionEvent) throws IOException {
+        screenManager.loadStage(rootPane, "/aatoets/view/startScene.fxml");
+    }
+
+    public void genereerToets(ActionEvent actionEvent) {
+        RadioButton selectedRadionbutton = (RadioButton) aantal.getSelectedToggle();
+        String toggleGroupValue = selectedRadionbutton.getText();
+    }
+
+    public void afsluiten(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
