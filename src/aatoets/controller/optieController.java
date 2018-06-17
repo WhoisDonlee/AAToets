6a8a3669fa9143cc322223244eb2eef39dce640e
Dennis Dollee <dennisdollee@gmail.com>
2018-06-17 15:43:58 +0200

    OptieController speelbutton enable/disable + checkbox eventlisteners

--- src/aatoets/controller/optieController.java
+++ src/aatoets/controller/optieController.java
@@ -1,14 +1,12 @@
 package aatoets.controller;
 
+import aatoets.data.OptieClass;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
@@ -18,10 +16,15 @@ import java.util.ResourceBundle;
 
 public class optieController extends Controller implements Initializable {
 
-    String[] comboBoxList = {"Volledige Naam", "1 lettercode",
+    private String[] comboBoxList = {"Volledige Naam", "1 lettercode",
                              "3 lettercode", "Hydrofobiciteit",
                              "Lading", "Grootte",
                              "3D Voorkeur", "Structuur"};
+    private int vragenlijstTrue = 0;
+    private int antwoordenlijstTrue = 0;
+
+    @FXML
+    Button speelButton;
     @FXML
     TextField naamTextField;
     @FXML
@@ -30,53 +33,55 @@ public class optieController extends Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         naamTextFieldChangeListener();
+        createCheckboxes(0);
+        createCheckboxes(1);
+    }
+
+    private void createCheckboxes(int col) {
         for (int i = 0; i < comboBoxList.length; i++) {
             CheckBox cb;
-            checkBoxGrid.add(cb = new CheckBox(comboBoxList[i]),0 ,i+1);
+            checkBoxGrid.add(cb = new CheckBox(comboBoxList[i]), col ,i+1);
             cb.setId("cb" + i);
-            cb.selectedProperty().addListener(new ChangeListener<Boolean>() {
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
-                    System.out.println(t1);
+            cb.selectedProperty().addListener((observableValue, aBoolean, t1) -> {
+                if (col == 0) {
+                    vragenlijstTrue += t1 ? +1 : -1;
+                } else if (col == 1) {
+                    antwoordenlijstTrue += t1 ? +1 : -1;
                 }
+                enableSpeelButton();
             });
 
         }
-
     }
 
-    private void onCheck(CheckBox cb) {
-        System.out.println(cb.getId());
+    private void enableSpeelButton() {
+        if ((vragenlijstTrue == 0) || (antwoordenlijstTrue == 0) || (naamTextField.getText().equals(""))) {
+            speelButton.setDisable(true);
+        } else {
+           speelButton.setDisable(false);
+        }
+    }
+
+    private void slaOptiesOp() {
+        OptieClass.setNaam(naamTextField.getText());
+    }
+
+    private void naamTextFieldChangeListener() {
+        naamTextField.textProperty().addListener((observableValue, s, t1) -> enableSpeelButton());
     }
 
     public void loadSpeelScene(ActionEvent actionEvent) throws IOException {
-        System.out.println(getNaamTextField());
+        slaOptiesOp();
 //        screenManager.loadStage(rootPane, "/aatoets/view/speelScene.fxml");
     }
 
     public void loadBeginScene(ActionEvent actionEvent) throws IOException {
-        System.out.println(getNaamTextField());
-//        screenManager.loadStage(rootPane, "/aatoets/view/startScene.fxml");
+        screenManager.loadStage(rootPane, "/aatoets/view/startScene.fxml");
     }
 
     public void afsluiten(ActionEvent actionEvent) {
+        System.exit(0);
     }
 
-    private void slaOptiesOp() {
-       getNaamTextField();
-    }
-
-    private void naamTextFieldChangeListener() {
-        naamTextField.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                System.out.println(getNaamTextField());
-            }
-        });
-    }
-
-    private String getNaamTextField() {
-       return naamTextField.getText();
-    }
 
 }
